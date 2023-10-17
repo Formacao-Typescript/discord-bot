e7912b50c14226a00b6a071962801b2aafc3f994
Rogério Munhoz <rogerio.munhoz@pipefy.com>
2023-10-17 11:16:46 -0300

    debug: add more logs

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,7 @@
+// deno-lint-ignore-file no-explicit-any
 /// <reference lib="deno.unstable" />
 import { debug, serve, serveStatic } from "deps.ts";
 import { handleInteraction } from "./bot.ts";
-
 import { getConfig } from "./util/config.ts";
 import { handleWebhookRequest } from "./webhook/webhook.ts";
 import { emailQueueWorker } from "./workers/email.ts";
@@ -11,7 +11,9 @@ const config = await getConfig();
 const kv = await Deno.openKv();
 
 kv.listenQueue(async (message) => {
+  log(`Received message of type ${(message as any).type} from queue`);
   await emailQueueWorker(message);
+  log(`Processed message of type ${(message as any).type} from queue`);
 });
 
 serve({
